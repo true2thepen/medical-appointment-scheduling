4a99ad539481537444218587bfabbe75564d6304
Sebastian Haas <sebastian@haas.tech>
2016-08-18 01:34:24 +0200

    Fixed karma unit tests according to breaking changes introduced in rc5.
    Signed-off-by: Sebastian Haas <sebastian@haas.tech>

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,21 +1,16 @@
-import {
-  beforeEachProviders,
-  inject,
-  it
-} from '@angular/core/testing';
+import { addProviders, inject } from '@angular/core/testing';
 
 // Load the implementations that should be tested
-import { App } from './app.component';
+import { AppComponent } from './app.component';
 import { AppState } from './app.service';
 
-describe('App', () => {
-  // provide our implementations or mocks to the dependency injector
-  beforeEachProviders(() => [
-    AppState,
-    App
-  ]);
+describe('AppComponent and AppState', () => {
 
-  it('should have a url', inject([ App ], (app) => {
+  beforeEach(() => {
+    addProviders([AppComponent, AppState]);
+  });
+
+  it('should have a url', inject([ AppComponent ], (app) => {
     expect(app.url).toEqual('https://twitter.com/AngularClass');
   }));
 
