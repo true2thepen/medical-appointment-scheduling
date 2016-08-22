af1bae9557b6a42f23f1922bd37956629f0f5b08
Sebastian Haas <sebastian@haas.tech>
2016-08-22 23:43:36 +0200

    Updated to zone.js 0.6.16 with respective changes in config/spec-bundle.js
    Signed-off-by: Sebastian Haas <sebastian@haas.tech>

--- config/spec-bundle.js
+++ config/spec-bundle.js
@@ -24,7 +24,7 @@ require('zone.js/dist/long-stack-trace-zone');
 require('zone.js/dist/async-test');
 require('zone.js/dist/fake-async-test');
 require('zone.js/dist/sync-test');
-require('zone.js/dist/proxy-zone'); // since zone.js 0.6.14
+require('zone.js/dist/proxy'); // since zone.js 0.6.15
 require('zone.js/dist/jasmine-patch'); // put here since zone.js 0.6.14
 
 // RxJS
