fa4f435ad6e9a5d08ee2ef45adf9425df4a648d5
Sebastian Haas <sebastian@haas.tech>
2016-11-07 04:37:42 +0100

    Check UTC/local times are displayed correctly in entire app #113
    Signed-off-by: Sebastian Haas <sebastian@haas.tech>

---
+++ bin/third-party-fixes.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# primeng schedule component
+cp third-party-fixes/primeng/components/schedule/* node_modules/primeng/components/schedule/
+
+echo "Patched third-party libraries.\n"
