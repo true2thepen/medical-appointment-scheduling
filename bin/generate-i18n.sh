d9eec229f69d84a3943ccea9673d7b100ee3e4be
Sebastian Haas <sebastian@haas.tech>
2016-11-10 15:21:40 +0100

    Added script for generating i18n sources.
    Signed-off-by: Sebastian Haas <sebastian@haas.tech>

---
+++ bin/generate-i18n.sh
@@ -0,0 +1,18 @@
+# 1) Have to comment out all styleUrl declarations
+for i in src/app/*.component.ts; do
+    sed -i '' 's/\(styleUrls:.*\)/\/\/ \1/g' "$i";
+done
+for i in src/app/appointment/*.component.ts; do
+    sed -i '' 's/\(styleUrls:.*\)/\/\/ \1/g' "$i";
+done
+
+# 2) Run ng-x18n
+npm run i18n
+
+# 3) Remove comments from all styleUrl declarations (undo 1) )
+for i in src/app/*.component.ts; do
+    sed -i '' 's/\(\/\/ \)\(styleUrls:.*\)/\2/g' "$i";
+done
+for i in src/app/appointment/*.component.ts; do
+    sed -i '' 's/\(\/\/ \)\(styleUrls:.*\)/\2/g' "$i";
+done
