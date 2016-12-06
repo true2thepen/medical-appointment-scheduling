2cb17f372a4a9e895ca6bc5f4ddf87fcc5fc489a
Sebastian Haas <sebastian@haas.tech>
2016-12-06 14:39:57 +0100

    Fixed lint problems.
    Signed-off-by: Sebastian Haas <sebastian@haas.tech>

--- src/app/appointment/clockpicker/clockpicker.component.ts
+++ src/app/appointment/clockpicker/clockpicker.component.ts
@@ -1,9 +1,12 @@
-import { Component, ElementRef, OnDestroy, DoCheck, Input, Output, EventEmitter, IterableDiffers, AfterViewChecked, ViewChild, forwardRef } from '@angular/core';
-import { MdInput } from '@angular/material';
-import { NG_VALUE_ACCESSOR, ControlValueAccessor, FormControl, NG_VALIDATORS } from '@angular/forms';
+import { Component, ElementRef, OnDestroy, DoCheck,
+  Input, Output, EventEmitter, IterableDiffers,
+  AfterViewChecked, ViewChild, forwardRef }         from '@angular/core';
+import { MdInput }                                  from '@angular/material';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor,
+  FormControl, NG_VALIDATORS }                      from '@angular/forms';
 
-import * as ClockPicker from './clockpicker.js';
-import * as moment from 'moment';
+import * as ClockPicker                             from './clockpicker.js';
+import * as moment                                  from 'moment';
 
 export const CLOCKPICKER_VALUE_ACCESSOR: any = {
   provide: NG_VALUE_ACCESSOR,
@@ -25,13 +28,13 @@ export function validateTime(c: FormControl) {
     };
     return err;
   }
-}
+};
 
 export const CLOCKPICKER_VALIDATORS: any = {
   provide: NG_VALIDATORS,
   useValue: validateTime,
   multi: true
-}
+};
 
 @Component({
   selector: 'clockpicker',
@@ -61,11 +64,48 @@ export class ClockPickerComponent implements AfterViewChecked, OnDestroy, Contro
   @Input() twelveHours: boolean;
   @Input() autoClose: boolean;
   @Input() doneText: string;
-  @ViewChild('clockPicker') private el:MdInput;
+  @ViewChild('clockPicker') private el: MdInput;
   private clockPicker: any;
   private _value: any = '';
   private input: HTMLInputElement;
   private initialized: boolean;
+
+  constructor() {
+    this.initialized = false;
+  }
+
+  public ngAfterViewChecked() {
+    if (!this.initialized && this.el._inputElement.nativeElement.offsetParent) {
+      this.initialize();
+    }
+  }
+
+  public ngOnDestroy() {
+    this.initialized = false;
+  }
+
+  public writeValue(value: any): void {
+    this.value = value;
+
+    if (this.input) {
+      if (this.value === undefined || this.value === null) {
+        this.input.value = '';
+      }
+    }
+  }
+
+  public registerOnChange(fn: Function): void {
+    this.onModelChange = fn;
+  }
+
+  public registerOnTouched(fn: Function): void {
+    this.onModelTouched = fn;
+  }
+
+  public setDisabledState(val: boolean): void {
+    this.disabled = val;
+  }
+
   private onModelChange: Function = () => {};
   private onModelTouched: Function = () => {};
 
@@ -77,73 +117,37 @@ export class ClockPickerComponent implements AfterViewChecked, OnDestroy, Contro
     }
   }
 
-  constructor() {
-    this.initialized = false;
-  }
-
-  ngAfterViewChecked() {
-    if (!this.initialized && this.el._inputElement.nativeElement.offsetParent) {
-      this.initialize();
-    }
-  }
-
-  initialize() {
+  private initialize() {
     this.input = this.el._inputElement.nativeElement;
     this.clockPicker = new ClockPicker(this.input, {
-      'default': '',                                    // default time, 'now' or '13:14' e.g.
-      fromnow: 0,                                       // set default time to * milliseconds from now (using with default = 'now')
-      donetext: this.doneText ? this.doneText : 'Done', // done button text
-      autoclose: this.autoClose,                        // auto close when minute is selected
-      ampmclickable: false,                             // set am/pm button on itself
-      darktheme: false,                                 // set to dark theme
-      twelvehour: this.twelveHours,                     // change to 12 hour AM/PM clock from 24 hour
-      vibrate: true,                                    // vibrate the device when dragging clock hand
-      afterDone: () => { this.afterClockPickerDone() }
+      'default': '',
+      fromnow: 0,
+      donetext: this.doneText ? this.doneText : 'Done',
+      autoclose: this.autoClose,
+      ampmclickable: false,
+      darktheme: false,
+      twelvehour: this.twelveHours,
+      vibrate: true,
+      afterDone: () => { this.afterClockPickerDone(); }
     });
     this.initialized = true;
   }
 
-  ngOnDestroy() {
-    this.initialized = false;
-  }
-
-  writeValue(value: any) : void {
-    this.value = value;
-
-    if (this.input) {
-      if (this.value == undefined || this.value == null) {
-        this.input.value = '';
-      }
-    }
-  }
-
-  registerOnChange(fn: Function): void {
-    this.onModelChange = fn;
-  }
-
-  registerOnTouched(fn: Function): void {
-    this.onModelTouched = fn;
-  }
-
-  setDisabledState(val: boolean): void {
-    this.disabled = val;
-  }
-
-  show() {
+  private show() {
     this.clockPicker.show();
   }
 
-  afterClockPickerDone() {
+  private afterClockPickerDone() {
     // Hotfix to notify md-input that its value has changed
     this.sanitizeApplyTime(this.input.value);
     this.input.value = this.value;
   }
 
-  onKey(event: Event) {
+  private onKey(event: Event) {
     this.value = (this.input.value);
   }
 
-  onBlur(event: Event) {
+  private onBlur(event: Event) {
     this.sanitizeApplyTime(this.input.value);
   }
 
