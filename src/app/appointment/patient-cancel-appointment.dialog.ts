c28b79ac72d89475c1b73c6fac2f23919f1d3897
Sebastian Haas <sebastian@haas.tech>
2017-03-24 18:39:48 +0100

    update: theming for delete appointment dialog
    Signed-off-by: Sebastian Haas <sebastian@haas.tech>

--- src/app/appointment/patient-cancel-appointment.dialog.ts
+++ src/app/appointment/patient-cancel-appointment.dialog.ts
@@ -7,7 +7,8 @@ import { MdDialogRef } from '@angular/material';
   <p i18n>Do you really want to cancel this appointment?</p>
   <button md-button i18n type="button" (click)="dialogRef.close('yes')">YES</button>
   <button md-button i18n type="button" (click)="dialogRef.close('no')">NO</button>
-  `
+  `,
+  styles: ['p { font-family: "Roboto"; }']
 })
 export class PatientCancelAppointmentDialogComponent {
   constructor(public dialogRef: MdDialogRef<PatientCancelAppointmentDialogComponent>) {}
